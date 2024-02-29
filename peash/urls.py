80f9ac3a778e39c1cc58db7606599a00b681d3a1
Saydul Mursalin Peash <saydulpeash428@gmail.com>
2024-02-29 08:35:59 +0600

    wrg

--- peash/urls.py
+++ peash/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('',Home,name='home_page'),
     path('resume/download/',download_resume,name="download_resume"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
